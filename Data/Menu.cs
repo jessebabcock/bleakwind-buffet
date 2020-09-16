92ce219a755d0034e68d361194c920a0675d4947
Jesse Babcock <jesseb98@ksu.edu>
2020-09-16 07:35:12 -0500

    updated for mistakes: fullmenu tests documentation

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -240,27 +240,143 @@ namespace BleakwindBuffet.Data
         {
             List<IOrderItem> Items = new List<IOrderItem>();
 
-            BriarheartBurger bb = new BriarheartBurger();
-            DoubleDraugr dd = new DoubleDraugr();
-            GardenOrcOmelette goo = new GardenOrcOmelette();
-            PhillyPoacher pp = new PhillyPoacher();
-            SmokehouseSkeleton ss = new SmokehouseSkeleton();
-            ThalmorTriple tt = new ThalmorTriple();
-            ThugsTBone ttb = new ThugsTBone();
-            Items.Add(bb);
-            Items.Add(dd);
-            Items.Add(goo);
-            Items.Add(pp);
-            Items.Add(ss);
-            Items.Add(tt);
-            Items.Add(ttb);
+            AretinoAppleJuice ajs = new AretinoAppleJuice();
+            ajs.Size = Size.Small;
+            AretinoAppleJuice ajm = new AretinoAppleJuice();
+            ajm.Size = Size.Medium;
+            AretinoAppleJuice ajl = new AretinoAppleJuice();
+            ajl.Size = Size.Large;
+            Items.Add(ajs);
+            Items.Add(ajm);
+            Items.Add(ajl);
+
+            CandlehearthCoffee ccs = new CandlehearthCoffee();
+            ccs.Size = Size.Small;
+            CandlehearthCoffee ccm = new CandlehearthCoffee();
+            ccm.Size = Size.Medium;
+            CandlehearthCoffee ccl = new CandlehearthCoffee();
+            ccl.Size = Size.Large;
+            Items.Add(ccs);
+            Items.Add(ccm);
+            Items.Add(ccl);
+
+            CandlehearthCoffee dccs = new CandlehearthCoffee();
+            dccs.Size = Size.Small;
+            dccs.Decaf = true;
+            CandlehearthCoffee dccm = new CandlehearthCoffee();
+            dccm.Size = Size.Medium;
+            dccm.Decaf = true;
+            CandlehearthCoffee dccl = new CandlehearthCoffee();
+            dccl.Size = Size.Large;
+            dccl.Decaf = true;
+            Items.Add(dccs);
+            Items.Add(dccm);
+            Items.Add(dccl);
+
+            MarkarthMilk mms = new MarkarthMilk();
+            mms.Size = Size.Small;
+            MarkarthMilk mmm = new MarkarthMilk();
+            mmm.Size = Size.Medium;
+            MarkarthMilk mml = new MarkarthMilk();
+            mml.Size = Size.Large;
+            Items.Add(mms);
+            Items.Add(mmm);
+            Items.Add(mml);
+
+            SailorSoda sssbb = new SailorSoda();
+            sssbb.Size = Size.Small;
+            sssbb.Flavor = SodaFlavor.Blackberry;
+            SailorSoda ssmbb = new SailorSoda();
+            ssmbb.Size = Size.Medium;
+            ssmbb.Flavor = SodaFlavor.Blackberry;
+            SailorSoda sslbb = new SailorSoda();
+            sslbb.Size = Size.Large;
+            sslbb.Flavor = SodaFlavor.Blackberry;
+            Items.Add(sssbb);
+            Items.Add(ssmbb);
+            Items.Add(sslbb);
+
+            SailorSoda sssc = new SailorSoda();
+            sssc.Size = Size.Small;
+            sssc.Flavor = SodaFlavor.Cherry;
+            SailorSoda ssmc = new SailorSoda();
+            ssmc.Flavor = SodaFlavor.Cherry;
+            ssmc.Size = Size.Medium;
+            SailorSoda sslc = new SailorSoda();
+            sslc.Flavor = SodaFlavor.Cherry;
+            sslc.Size = Size.Large;
+            Items.Add(sssc);
+            Items.Add(ssmc);
+            Items.Add(sslc);
+
+            SailorSoda sssg = new SailorSoda();
+            sssg.Size = Size.Small;
+            sssg.Flavor = SodaFlavor.Grapefruit;
+            SailorSoda ssmg = new SailorSoda();
+            ssmg.Size = Size.Medium;
+            ssmg.Flavor = SodaFlavor.Grapefruit;
+            SailorSoda sslg = new SailorSoda();
+            sslg.Size = Size.Large;
+            sslg.Flavor = SodaFlavor.Grapefruit;
+            Items.Add(sssg);
+            Items.Add(ssmg);
+            Items.Add(sslg);
+
+            SailorSoda sssl = new SailorSoda();
+            sssl.Size = Size.Small;
+            sssl.Flavor = SodaFlavor.Lemon;
+            SailorSoda ssml = new SailorSoda();
+            ssml.Size = Size.Medium;
+            ssml.Flavor = SodaFlavor.Lemon;
+            SailorSoda ssll = new SailorSoda();
+            ssll.Size = Size.Large;
+            ssll.Flavor = SodaFlavor.Lemon;
+            Items.Add(sssl);
+            Items.Add(ssml);
+            Items.Add(ssll);
+
+            SailorSoda sssp = new SailorSoda();
+            sssp.Size = Size.Small;
+            sssp.Flavor = SodaFlavor.Peach;
+            SailorSoda ssmp = new SailorSoda();
+            ssmp.Size = Size.Medium;
+            ssmp.Flavor = SodaFlavor.Peach;
+            SailorSoda sslp = new SailorSoda();
+            sslp.Size = Size.Large;
+            sslp.Flavor = SodaFlavor.Peach;
+            Items.Add(sssp);
+            Items.Add(ssmp);
+            Items.Add(sslp);
+
+            SailorSoda sssw = new SailorSoda();
+            sssw.Size = Size.Small;
+            sssw.Flavor = SodaFlavor.Watermelon;
+            SailorSoda ssmw = new SailorSoda();
+            ssmw.Size = Size.Medium;
+            ssmw.Flavor = SodaFlavor.Watermelon;
+            SailorSoda sslw = new SailorSoda();
+            sslw.Size = Size.Large;
+            sslw.Flavor = SodaFlavor.Watermelon;
+            Items.Add(sssw);
+            Items.Add(ssmw);
+            Items.Add(sslw);
+
+            WarriorWater wws = new WarriorWater();
+            wws.Size = Size.Small;
+            WarriorWater wwm = new WarriorWater();
+            wwm.Size = Size.Medium;
+            WarriorWater wwl = new WarriorWater();
+            wwl.Size = Size.Large;
+            Items.Add(wws);
+            Items.Add(wwm);
+            Items.Add(wwl);
 
             DragonbornWaffleFries dwfs = new DragonbornWaffleFries();
             dwfs.Size = Size.Small;
             DragonbornWaffleFries dwfm = new DragonbornWaffleFries();
             dwfm.Size = Size.Medium;
             DragonbornWaffleFries dwfl = new DragonbornWaffleFries();
-            dwfm.Size = Size.Large;
+            dwfl.Size = Size.Large;
             Items.Add(dwfs);
             Items.Add(dwfm);
             Items.Add(dwfl);
@@ -295,105 +411,20 @@ namespace BleakwindBuffet.Data
             Items.Add(vsm);
             Items.Add(vsl);
 
-            AretinoAppleJuice ajs = new AretinoAppleJuice();
-            ajs.Size = Size.Small;
-            AretinoAppleJuice ajm = new AretinoAppleJuice();
-            ajm.Size = Size.Medium;
-            AretinoAppleJuice ajl = new AretinoAppleJuice();
-            ajl.Size = Size.Large;
-            Items.Add(ajs);
-            Items.Add(ajm);
-            Items.Add(ajl);
-
-            CandlehearthCoffee ccs = new CandlehearthCoffee();
-            ccs.Size = Size.Small;
-            CandlehearthCoffee ccm = new CandlehearthCoffee();
-            ccm.Size = Size.Medium;
-            CandlehearthCoffee ccl = new CandlehearthCoffee();
-            ccl.Size = Size.Large;
-            Items.Add(ccs);
-            Items.Add(ccm);
-            Items.Add(ccl);
-
-            MarkarthMilk mms = new MarkarthMilk();
-            mms.Size = Size.Small;
-            MarkarthMilk mmm = new MarkarthMilk();
-            mmm.Size = Size.Medium;
-            MarkarthMilk mml = new MarkarthMilk();
-            mml.Size = Size.Large;
-            Items.Add(mms);
-            Items.Add(mmm);
-            Items.Add(mml);
-
-            SailorSoda sssbb = new SailorSoda();
-            sssbb.Size = Size.Small;
-            SailorSoda ssmbb = new SailorSoda();
-            ssmbb.Size = Size.Medium;
-            SailorSoda sslbb = new SailorSoda();
-            sslbb.Size = Size.Large;
-            Items.Add(sssbb);
-            Items.Add(ssmbb);
-            Items.Add(sslbb);
-
-            SailorSoda sssc = new SailorSoda();
-            sssc.Size = Size.Small;
-            SailorSoda ssmc = new SailorSoda();
-            ssmc.Size = Size.Medium;
-            SailorSoda sslc = new SailorSoda();
-            sslc.Size = Size.Large;
-            Items.Add(sssc);
-            Items.Add(ssmc);
-            Items.Add(sslc);
-
-            SailorSoda sssg = new SailorSoda();
-            sssg.Size = Size.Small;
-            SailorSoda ssmg = new SailorSoda();
-            ssmg.Size = Size.Medium;
-            SailorSoda sslg = new SailorSoda();
-            sslg.Size = Size.Large;
-            Items.Add(sssg);
-            Items.Add(ssmg);
-            Items.Add(sslg);
-
-            SailorSoda sssl = new SailorSoda();
-            sssl.Size = Size.Small;
-            SailorSoda ssml = new SailorSoda();
-            ssml.Size = Size.Medium;
-            SailorSoda ssll = new SailorSoda();
-            ssll.Size = Size.Large;
-            Items.Add(sssl);
-            Items.Add(ssml);
-            Items.Add(ssll);
-
-            SailorSoda sssp = new SailorSoda();
-            sssp.Size = Size.Small;
-            SailorSoda ssmp = new SailorSoda();
-            ssmp.Size = Size.Medium;
-            SailorSoda sslp = new SailorSoda();
-            sslp.Size = Size.Large;
-            Items.Add(sssp);
-            Items.Add(ssmp);
-            Items.Add(sslp);
-
-            SailorSoda sssw = new SailorSoda();
-            sssw.Size = Size.Small;
-            SailorSoda ssmw = new SailorSoda();
-            ssmw.Size = Size.Medium;
-            SailorSoda sslw = new SailorSoda();
-            sslw.Size = Size.Large;
-            Items.Add(sssw);
-            Items.Add(ssmw);
-            Items.Add(sslw);
-
-            WarriorWater wws = new WarriorWater();
-            wws.Size = Size.Small;
-            WarriorWater wwm = new WarriorWater();
-            wwm.Size = Size.Medium;
-            WarriorWater wwl = new WarriorWater();
-            wwl.Size = Size.Large;
-            Items.Add(wws);
-            Items.Add(wwm);
-            Items.Add(wwl);
+            BriarheartBurger bb = new BriarheartBurger();
+            DoubleDraugr dd = new DoubleDraugr();
+            GardenOrcOmelette goo = new GardenOrcOmelette();
+            PhillyPoacher pp = new PhillyPoacher();
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            ThalmorTriple tt = new ThalmorTriple();
+            ThugsTBone ttb = new ThugsTBone();
+            Items.Add(bb);
+            Items.Add(dd);
+            Items.Add(goo);
+            Items.Add(pp);
+            Items.Add(ss);
+            Items.Add(tt);
+            Items.Add(ttb);
 
             return Items;
         }
--- DataTests/UnitTests/MenuTest.cs
+++ DataTests/UnitTests/MenuTest.cs
@@ -95,5 +95,77 @@ namespace BleakwindBuffet.DataTests.UnitTests
             Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Medium Vokun Salad"); });
             Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Large Vokun Salad"); });
         }
+
+        [Fact]
+        public void MenuShouldReturnAllMenuItems()
+        {
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Dragonborn Waffle Fries"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Dragonborn Waffle Fries"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Dragonborn Waffle Fries"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Fried Miraak"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Fried Miraak"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Fried Miraak"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Mad Otar Grits"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Mad Otar Grits"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Mad Otar Grits"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Vokun Salad"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Vokun Salad"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Vokun Salad"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Aretino Apple Juice"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Aretino Apple Juice"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Aretino Apple Juice"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Candlehearth Coffee"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Candlehearth Coffee"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Candlehearth Coffee"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Decaf Candlehearth Coffee"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Decaf Candlehearth Coffee"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Decaf Candlehearth Coffee"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Markarth Milk"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Markarth Milk"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Markarth Milk"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Cherry Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Cherry Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Cherry Sailor Soda"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Blackberry Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Blackberry Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Blackberry Sailor Soda"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Grapefruit Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Grapefruit Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Grapefruit Sailor Soda"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Lemon Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Lemon Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Lemon Sailor Soda"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Peach Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Peach Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Peach Sailor Soda"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Watermelon Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Watermelon Sailor Soda"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Watermelon Sailor Soda"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Small Warrior Water"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Medium Warrior Water"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Large Warrior Water"); });
+
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Briarheart Burger"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Double Draugr"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Garden Orc Omelette"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Philly Poacher"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Smokehouse Skeleton"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Thalmor Triple"); });
+            Assert.Contains(Menu.FullMenu(), (item) => { return item.ToString().Equals("Thugs T-Bone"); });
+        }
     }
 }
